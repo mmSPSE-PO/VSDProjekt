ec2962f072292101dea6600f403bdc625b01356c
Matej Mazúr <41269745+MatejMa2ur@users.noreply.github.com>
2021-03-03 19:23:36 +0100

    Added Data Anotations For Each Device

--- VSDProjekt/Model/zariadenie.cs
+++ VSDProjekt/Model/zariadenie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace VSDProjekt.Model
     public class zariadenie
     {
         public int zariadenieID { get; set; }
+        [MaxLength(200)]
         public string Nazov { get; set; }
+        [Range(0.0,220*20, ErrorMessage = "Pole musí byť väščie ako 0 a menšie ako 4400")]
         public double Spotreba { get; set; }
+        [Range(0.0, 24, ErrorMessage = "Pole musí byť väščie ako 0 a menšie ako 24")]
         public double Dlzka { get; set; }
 
         public string UserID { get; set; }
